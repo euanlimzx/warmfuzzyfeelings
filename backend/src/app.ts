c6fbbf12a843ec271c412bdfe3c8f4bfb807ae61
SleepyWoodpecker <shawnweichew@gmail.com>
2025-05-31 11:40:55 -0700

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { getAWSSignedUrl } from "./utils/mediaHandler";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
   res.status(200).send("helloi");
