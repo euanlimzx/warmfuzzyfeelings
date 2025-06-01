d8521b1d5982791020fe450f9d14ea6e816394bd
euanlimzx <euanlimzx@gmail.com>
2025-05-31 17:07:18 -0700

    removed unused imports

--- frontend/src/pages/card/[id].tsx
+++ frontend/src/pages/card/[id].tsx
@@ -1,11 +1,9 @@
-import { ConfettiFireworks } from "@/components/countdown/confetti";
 import { Countdown } from "@/components/countdown/countdown";
 import { Memories } from "@/components/memories/memories";
 
 export default function Card() {
   return (
     <div className="h-full">
-      <ConfettiFireworks />
       {/* <Memories /> */}
       <Countdown />
     </div>
