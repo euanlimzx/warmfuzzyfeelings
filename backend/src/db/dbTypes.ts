88476618b012c69e3c5dfbd223936f6aa2cdfeb5
SleepyWoodpecker <shawnweichew@gmail.com>
2025-06-19 11:01:22 +0800

    updated db types to support multiple image uploads

--- backend/src/db/dbTypes.ts
+++ backend/src/db/dbTypes.ts
@@ -58,6 +58,7 @@ export type Database = {
           final_message_response: string | null
           id: string
           image_url: string | null
+          image_urls: string[]
           memory_response: string | null
           responder_name: string | null
         }
@@ -68,6 +69,7 @@ export type Database = {
           final_message_response?: string | null
           id: string
           image_url?: string | null
+          image_urls?: string[]
           memory_response?: string | null
           responder_name?: string | null
         }
@@ -78,6 +80,7 @@ export type Database = {
           final_message_response?: string | null
           id?: string
           image_url?: string | null
+          image_urls?: string[]
           memory_response?: string | null
           responder_name?: string | null
         }
