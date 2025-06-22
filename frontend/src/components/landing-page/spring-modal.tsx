9dbf4828ddc65cbdee78ec4283b07397afa07864
euanlimzx <euanlimzx@gmail.com>
2025-06-22 17:11:02 +0800

    scaffolding for create a card

--- frontend/src/components/landing-page/spring-modal.tsx
+++ frontend/src/components/landing-page/spring-modal.tsx
@@ -1,8 +1,6 @@
-import axios from "axios";
-import { motion } from "framer-motion";
-import { AnimatePresence } from "framer-motion";
-import { useState, useRef, useEffect } from "react";
-import { FormSubmissionErrorNotification } from "../question-form/Notifications";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRef, useEffect } from "react";
+import CreateCardForm from "./create-card-form";
 
 interface SpringModalProps {
   isOpen: boolean;
@@ -10,9 +8,6 @@ interface SpringModalProps {
 }
 
 const SpringModal = ({ isOpen, setIsOpen }: SpringModalProps) => {
-  const [email, setEmail] = useState("");
-  const [isValidEmail, setIsValidEmail] = useState(true);
-  const [showSuccessNotif, setShowSuccessNotif] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -39,35 +34,6 @@ const SpringModal = ({ isOpen, setIsOpen }: SpringModalProps) => {
     };
   }, [isOpen, setIsOpen]);
 
-  const validateEmail = (email: string) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  };
-
-  const handleSubmit = async () => {
-    if (!validateEmail(email)) {
-      setIsValidEmail(false);
-      return;
-    }
-
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/join-waitlist`,
-      {
-        email: email,
-      }
-    );
-    console.log(response);
-    setShowSuccessNotif(true);
-    setTimeout(() => {
-      setShowSuccessNotif(false);
-      setIsOpen(false);
-    }, 1000);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    setIsValidEmail(true); // Reset validation on change
-  };
-
   return (
     <AnimatePresence>
       {isOpen && (
@@ -77,43 +43,9 @@ const SpringModal = ({ isOpen, setIsOpen }: SpringModalProps) => {
             initial={{ scale: 0, rotate: "12.5deg" }}
             animate={{ scale: 1, rotate: "0deg" }}
             exit={{ scale: 0, rotate: "0deg" }}
-            className="bg-red-600 text-white p-10 w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
+            className="w-full max-w-lg shadow-xl cursor-default relative overflow-hidden"
           >
-            {showSuccessNotif && (
-              <FormSubmissionErrorNotification
-                errorMessage="Sick! Thanks for the support 🎉"
-                removeNotif={() => setShowSuccessNotif(false)}
-              />
-            )}
-            <div className="relative z-10">
-              <h3 className="text-3xl font-bold text-center mb-2">
-                We&apos;ll let you know when we launch!
-              </h3>
-              <div className="flex flex-col sm:flex-row gap-2">
-                <div className="w-full">
-                  <input
-                    type="email"
-                    placeholder="Enter your email"
-                    className={`w-full p-2 bg-white border-3 border-black rounded-none focus:outline-none focus:ring-0 focus:border-black text-black caret-black ${
-                      !isValidEmail ? "border-red-500" : ""
-                    }`}
-                    value={email}
-                    onChange={handleEmailChange}
-                  />
-                </div>
-                <button
-                  onClick={handleSubmit}
-                  className="w-full sm:w-fit px-6 py-2 font-medium border-2 border-black bg-white text-black transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]"
-                >
-                  Submit
-                </button>
-              </div>
-              {!isValidEmail && (
-                <p className="text-white text-sm mt-1 font-medium">
-                  Invalid email address
-                </p>
-              )}
-            </div>
+            <CreateCardForm setIsOpen={setIsOpen} />
           </motion.div>
         </div>
       )}
